0e6fd6e83a730d4ab4d6f48ee7dfb2d94b30faea
Jamie <jxxthomson@gmail.com>
2022-11-18 22:45:04 +0000

    Got the code errors tho, doesn tunderstand fast api file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def playerPrices():
+    return {"Theo": 9.3}
+
+
+@app.post('/')
+def changePlayerPrices():
+    return {"Succes you changed this players price": 8.2}
